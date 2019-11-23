76eab8e703a9d7def05ef08f2defb9d1f3535609
Usamagill1998 <41861946+Usamagill1998@users.noreply.github.com>
2019-11-23 13:55:49 +0500

    second

---
+++ config/Firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Rename this file to "firebaeConfig.js" before use
+// Replace all Xs with real Firebase API keys
+
+export default {
+  apiKey: "AIzaSyDds36RKlAZCrG6s3uCmZYbbxj9mhz--MM",
+  authDomain: "eventwebapp-196bc.firebaseapp.com",
+  databaseURL: "https://eventwebapp-196bc.firebaseio.com",
+  projectId: "eventwebapp-196bc",
+  storageBucket: "eventwebapp-196bc.appspot.com",
+  messagingSenderId: "733215706681",
+  appId: "1:733215706681:web:c0db766a425e47ba5147e8",
+  measurementId: "G-E1DD1B60HB"
+}
